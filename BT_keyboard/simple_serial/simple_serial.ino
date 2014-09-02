c7bad5c4691cb2c8b33f88209d4ac1a0891ec05f
rich-hart <richhohart@gmail.com>
2014-09-02 19:26:14 -0400

    working setch

--- BT_keyboard/simple_serial/simple_serial.ino
+++ BT_keyboard/simple_serial/simple_serial.ino
@@ -1,18 +1,28 @@
-int incomingByte = 0;   // for incoming serial data
-String txtMsg = "";
+
+
+#include <SoftwareSerial.h>
+ 
+SoftwareSerial BT = SoftwareSerial(3, 2);
+//int incomingByte = 0;   // for incoming serial data
+//String txtMsg = "";
 
 void setup() {
-        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+    Serial.begin(9600);
+    BT.begin(9600);
+        //Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
 }
 
 void loop(){
    //
-  //delay(1000);
+  delay(1000);
   Serial.println("Ready");
   
-  //while (! Serial.available());
+ // while (! Serial.available());
     while (Serial.available() > 0) {
-      Serial.write(Serial.read());
+      char c =Serial.read();
+      Serial.write(c);
+          BT.write(c);
+          delay(10);
     //char inChar = Serial.read();
     //txtMsg += inChar;
   } 
